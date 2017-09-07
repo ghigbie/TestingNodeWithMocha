51bf4d3d5095d55258ecfb450859cf01cc84bb14
George Higbie <gehigbie@gmail.com>
2017-09-07 14:17:00 +0000

    Add new test case

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,7 +2,14 @@ const utils = require('./utils');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
-    if( res !== 44){
+    if(res !== 44){
         throw new Error(`Expected 44, but got ${res}.`);
+    }
+});
+
+it('should sqaure a number', () => {
+    var res = utils.square(7);
+    if(res !== 49){
+        throw new Error(`Expected 49, but got ${res}.`);
     }
 });
