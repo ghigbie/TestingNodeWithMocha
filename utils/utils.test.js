649fcadd0bbb135000eb33486d84bf21f5b44f3e
George Higbie <gehigbie@gmail.com>
2017-09-07 17:21:14 +0000

    Replace if statments with expect library calls

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -6,16 +6,10 @@ const utils = require('./utils');
 
 it('should add two numbers', () => {
     let res = utils.add(33, 11);
-    //expect(res).toBe(44);
-    expect(2).toBeA('number');
-    // if(res !== 44){
-    //     throw new Error(`Expected 44, but got ${res}.`);
-    // }
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('should sqaure a number', () => {
     let  res = utils.square(7);
-    if(res !== 49){
-        throw new Error(`Expected 49, but got ${res}.`);
-    }
+    expect(res).toBe(49).toBeA('number');
 });
