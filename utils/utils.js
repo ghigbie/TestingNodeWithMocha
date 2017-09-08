9f33d651cdff97689d95b0da7df1f5cedbf26934
George Higbie <gehigbie@gmail.com>
2017-09-08 13:34:10 +0000

    Add new test case

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,9 @@
 let add = (a, b) => a + b;
 let square = (x) => x * x;
+let subtract =(a, b) => a - b;
 
 module.exports = {
     add,
-    square
+    square,
+    subtract
 };
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -13,3 +13,12 @@ it('should sqaure a number', () => {
     let  res = utils.square(7);
     expect(res).toBe(49).toBeA('number');
 });
+
+it('should subtract a number', () => {
+    let res = utils.subtract(5,3);
+    expect(res).toBe(2).toBeA('number');
+});
+
+it('should expect some values', () => {
+    expect(!12).toNotBe(12);
+});
