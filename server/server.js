6462c7246b0ed705e8949ceaad07b22c2a4d97ca
George Higbie <gehigbie@gmail.com>
2017-09-11 14:32:21 +0000

    Add a root route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,11 @@ const express = require('express'),
 let PORT = process.env.PORT || 8082;
 let IP = process.env.IP;
 
+app.get('/', (req, res) => {
+    console.log('The root route is called');
+    res.send('Welcome to the root');
+});
+
 app.listen(PORT, IP, () => {
    console.log(`The server is listening on port ${PORT}.`); 
 });
