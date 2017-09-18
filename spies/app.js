246b3541d322fed330f150829d35ed0834fe51a6
George Higbie <gehigbie@gmail.com>
2017-09-18 19:42:42 +0000

    Add funcation and export statements

--- spies/app.js
+++ spies/app.js
@@ -1,3 +1,11 @@
-module.exports.handleSignup = (email, password) => {
-      
+const db = require('./db.js');
+
+let handleSignup = (email, password) =>{
+    //check if email already exists
+    db.saveUser({email, password});
+    //send the welcome email
+};
+
+module.exports = {
+    handleSignup
 };
